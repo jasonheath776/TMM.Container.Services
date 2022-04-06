2b1b452e40c3e389842f8cf10c653b7e53caf153
NITRO\Jason <jasonheath776@gmail.com>
2022-04-06 14:37:41 +0100

    controller

--- Customer.Domain/SeedWork/IRepository.cs
+++ Customer.Domain/SeedWork/IRepository.cs
@@ -4,5 +4,4 @@
     {
         IUnitOfWork UnitOfWork { get; }
     }
-
 }
---
+++ Customers.API/Controllers/CustomerController.cs
@@ -0,0 +1,52 @@
+﻿using Customers.API.Application.Commands;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Customers.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ILogger<CustomerController> _logger;
+        private readonly IMediator _mediator;
+
+        public CustomerController(
+             IMediator mediator,
+             ILogger<CustomerController> logger)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<Customer>> CreateCustomerAsync([FromBody] CreateCustomerCommand customer)
+        {          
+            Customer commandResult = await _mediator.Send(customer);
+           
+            if (commandResult == null)
+            {
+                return BadRequest();
+            }
+
+            return Created(Request.Path, commandResult);
+        }
+
+        [HttpPut]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<Customer>> UpdateCustomerAsync([FromBody] UpdateCustomerCommand customer)
+        {
+            Customer commandResult = await _mediator.Send(customer);
+
+            if (commandResult == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(commandResult);
+        }
+    }
+}
